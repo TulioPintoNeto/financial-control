67eea81357bb5db0ca8eef2cb1965a3e640c097c
Tulio Pinto Neto <tulio_neto_2@hotmail.com>
2024-04-12 22:28:22 +0100

    chore: setup path for environments file

--- src/app/core/initialization.ts
+++ src/app/core/initialization.ts
@@ -1,6 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth, GoogleAuthProvider } from 'firebase/auth';
-import { environment } from '../../environments/environment';
+import { environment } from 'env';
 
 const initialization = () => {
   const app = initializeApp(environment.firebaseConfig);
